9e89befac982ae24505bb5b60bbc72992676752e
Thiago Firsen <thiagofirsen47@hotmail.com>
2024-06-27 00:17:30 -0300

    chore: change default color

--- SwiftParaTodos/SceneDelegate.swift
+++ SwiftParaTodos/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let window = UIWindow(windowScene: windowScene)
         let viewController = UIViewController()
-        viewController.view.backgroundColor = .yellow
+        viewController.view.backgroundColor = .red
         
         let navigation = UINavigationController(rootViewController: viewController)
         
